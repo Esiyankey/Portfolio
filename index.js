1d724064f94a7ba48c312dc0c297a2863d41f7aa
Esi Yankey <graceyan244@gmail.com>
2023-07-12 14:01:51 +0000

    some changes made

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+const formFill = document.getElementById("form");
+const submit = document.getElementById("submit-button");
+const links = document.querySelectorAll("links");
+const reset = documetn.getElementById("reset-button");
+const inputReset = document.querySelectorAll(".input");
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("Form submitted");
+});
+
+resetButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  form.reset();
+});
 
 
-document.addEventListener('DOMContentLoaded', function() {
-const links = document.querySelectorAll('#links-ul a');
 
-
-links.forEach(link => {
-  link.addEventListener('click', function(event) {
-    event.preventDefault(); 
-    links.forEach(link => link.classList.remove('active'));
-    this.classList.add('active');
-    const href = this.getAttribute('data-href');
-    navigateToPage(href);
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      links.forEach((link) => link.classList.remove("active"));
+      this.classList.add("active");
+    });
   });
-});
-
-function navigateToPage(href) {
-  window.location.href = href;
-}
-});
